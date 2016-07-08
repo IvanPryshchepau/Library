b836ecfb34fee8b6e0e26ec8ed23207e1e1b8871
ivanpryshchepau <ivan.prishhepov@gmail.com>
2016-07-08 13:10:46 +0300

    block user

--- src/by/tr/library/dao/impl/SQLAdminDao.java
+++ src/by/tr/library/dao/impl/SQLAdminDao.java
@@ -4,12 +4,41 @@ import by.tr.library.bean.Book;
 import by.tr.library.dao.AdminDao;
 import by.tr.library.dao.exception.DAOException;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SQLAdminDao implements AdminDao {
 
 	@Override
-	public boolean blockUser(String login) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean blockUser(String login) throws DAOException {
+		try (BufferedReader reader = new BufferedReader(new FileReader("Users.txt"))){
+
+			String line;
+			List<String[]> lines = new ArrayList<String[]>();
+			while ((line = reader.readLine()) != null) {
+				String[] result = line.split("((^user)=')|(', (password|status)=')|('$)");
+				lines.add(result);
+			}
+
+			boolean status = false;
+			BufferedWriter writer = new BufferedWriter(new FileWriter("Users.txt"));
+			for (String[] field : lines) {
+				if (login.equals(field[1])) {
+					field[3] = "blocked";
+					status = true;
+				}
+				writer.write("user='" + field[1] + "', password='" + field[2] + "', status='" + field[3] + "'");
+				writer.append('\n');
+			}
+
+			writer.flush();
+
+			return status;
+		} catch (IOException e) {
+			throw new DAOException("DAO message", e);
+		}
+
 	}
 
 	@Override
